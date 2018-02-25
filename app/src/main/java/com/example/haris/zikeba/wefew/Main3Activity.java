f00616e268c313a7582af8c58823ec89cb29524b
haris14Git <haris.vuk@live.com>
2018-02-25 20:01:02 +0100

    samo-to

---
+++ app/src/main/java/com/example/haris/zikeba/wefew/Main3Activity.java
@@ -0,0 +1,15 @@
+package com.example.haris.zikeba.wefew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.haris.zikeba.R;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
---
+++ app/src/main/java/com/example/haris/zikeba/wefew/wefwefewf.java
@@ -0,0 +1,8 @@
+package com.example.haris.zikeba.wefew;
+
+/**
+ * Created by Haris on 2/25/2018.
+ */
+
+public class wefwefewf {
+}
