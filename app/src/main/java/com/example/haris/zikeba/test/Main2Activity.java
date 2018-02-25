b147d7f29aa81b259b0c032fd5aeedf685f9ae1c
haris14Git <haris.vuk@live.com>
2018-02-25 19:13:36 +0100

    first commit

---
+++ app/src/main/java/com/example/haris/zikeba/test/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.haris.zikeba.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.haris.zikeba.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
